7ef3be590709f2661799d0dd4c2a0cb81d120f42
fernanduandrade <onanduandrade@gmail.com>
2021-06-01 21:48:34 -0300

    add CalcularPeca app

---
+++ CalcularPecas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CalcularPecas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo, digite a quantidade de peças número 1: ");
+            float peca1Quantidade = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Agora digite o valor individual da peça número 1: ");
+            float peca1Preco = float.Parse(Console.ReadLine());
+            Console.WriteLine(peca1Preco);
+
+            Console.WriteLine("Digite agora a quantidade de peças número 2: ");
+            float peca2Quantidade = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Agora digite o valor individual da peça número 2: ");
+            float peca2Preco = float.Parse(Console.ReadLine());
+
+            float resultado = peca1Quantidade * peca1Preco  + peca2Quantidade * peca2Preco;
+
+            Console.WriteLine();
+            
+            Console.WriteLine($"VALOR A PAGAR: R$ {resultado:N2}");
+
+        
+        }
+    }
+}
