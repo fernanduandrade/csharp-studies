f365fba760a6ff0a841ea1e30a7c5f2dd4c8f7b6
fernanduandrade <onanduandrade@gmail.com>
2021-06-02 22:17:46 -0300

    adicionar estrutura de repeticao

---
+++ EstruturaRepeticao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EstruturaRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int password = 2002;
+            
+
+            
+            while (true)
+            {
+                Console.WriteLine("Digite a senha");
+                int passwordInput = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if(passwordInput != password)
+                {
+                    Console.WriteLine("Senha Invalida");
+                    Console.WriteLine();
+                }
+                else 
+                {
+                    Console.WriteLine("Acesso Permitido");
+                    break;
+                }
+            }
+        }
+    }
+}
