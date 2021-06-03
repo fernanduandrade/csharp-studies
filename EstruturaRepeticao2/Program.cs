353f3abc957808bec8169d5020d9197fe0384f3a
fernanduandrade <onanduandrade@gmail.com>
2021-06-02 22:29:20 -0300

    adicionar estrutura de repetição 2

---
+++ EstruturaRepeticao2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EstruturaRepeticao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inArray = 0;
+            int outArray = 0;
+
+            for(int i = 0; i < args.Length; i++) 
+            {
+                if(int.Parse(args[i]) >=10 && int.Parse(args[i]) <=20)
+                {
+                    inArray++;
+                }
+                else 
+                {
+                    outArray++;
+                }
+            }
+
+            Console.WriteLine($"{inArray} in, {outArray} out");
+        }
+    }
+}
