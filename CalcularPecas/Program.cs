541ee95f0d8ac91a6055ec1b2e7a270b7bb8db8d
fernanduandrade <onanduandrade@gmail.com>
2021-06-03 10:05:07 -0300

    refatorar CalcularPecas

--- CalcularPecas/Program.cs
+++ CalcularPecas/Program.cs
@@ -6,24 +6,14 @@ namespace CalcularPecas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bem vindo, digite a quantidade de peças número 1: ");
-            float peca1Quantidade = float.Parse(Console.ReadLine());
+            // Passe por argumentos os valores e quantidades de peça 1 e 2  
+            float peca1Quantidade = float.Parse(args[0]);
+            float peca1Preco = float.Parse(args[1]);   
+            float peca2Quantidade = float.Parse(args[2]);
+            float peca2Preco = float.Parse(args[3]);   
+            float valorTotal = peca1Quantidade * peca1Preco  + peca2Quantidade * peca2Preco;
 
-            Console.WriteLine("Agora digite o valor individual da peça número 1: ");
-            float peca1Preco = float.Parse(Console.ReadLine());
-            Console.WriteLine(peca1Preco);
-
-            Console.WriteLine("Digite agora a quantidade de peças número 2: ");
-            float peca2Quantidade = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Agora digite o valor individual da peça número 2: ");
-            float peca2Preco = float.Parse(Console.ReadLine());
-
-            float resultado = peca1Quantidade * peca1Preco  + peca2Quantidade * peca2Preco;
-
-            Console.WriteLine();
-            
-            Console.WriteLine($"VALOR A PAGAR: R$ {resultado:N2}");
+            Console.WriteLine($"VALOR A PAGAR: R$ {valorTotal:N2}");
 
         
         }
