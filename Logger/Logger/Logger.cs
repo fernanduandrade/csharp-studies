82182862f96b117c95d02e6ced4fbdca23d662bd
fernanduandrade <nando.jua@gmail.com>
2021-07-10 20:04:51 -0300

    Add logger project

---
+++ Logger/Logger/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Logger
+{
+    public static class Logger
+    {
+        public static void WriteLog(string message)
+        {
+            string logPath = "C:/Temp/log.txt";
+            using (StreamWriter write = new StreamWriter(logPath, true))
+            {
+                write.WriteLine($"{DateTime.Now} : {message}");
+            }
+        }
+    }
+}
