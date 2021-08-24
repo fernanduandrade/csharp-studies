fb8109578e9657704054dec3e19e4d577b1988dc
fernanduandrade <nando.jua@gmail.com>
2021-08-24 18:46:56 -0300

    fix log path

--- EmailValidation/EmailValidation/Utils/GenerateLog.cs
+++ EmailValidation/EmailValidation/Utils/GenerateLog.cs
@@ -7,7 +7,7 @@ namespace EmailValidation.Utils
     {
         public void WriteLog(string message)
         {
-            string logPath = $"/home/fernando/LOG-{DateTime.Now.ToString("dd-MM-yyyy")}.txt";
+            string logPath = $"C:\\temp\\logs\\LOG-{DateTime.Now.ToString("dd-MM-yyyy")}.txt";
             using (StreamWriter write = new StreamWriter(logPath, true))
             {
                 write.WriteLine($"{DateTime.Now} : {message}");
