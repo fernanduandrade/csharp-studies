79e1fb8d2f3a12741ed63bdbaf1546cce8b7f694
fernanduandrade <onanduandrade@gmail.com>
2021-09-05 11:06:34 -0300

    adicionado regra para torre

--- UdemyCourse/Xadrez/xadrez/Torre.cs
+++ UdemyCourse/Xadrez/xadrez/Torre.cs
@@ -11,6 +11,66 @@ namespace xadrez {
         public override string ToString()
         {
             return "T";
-                                       }
+
+        }
+
+        public bool PoderMover(Posicao posicao) {
+            Peca p = tabuleiro.peca(posicao);
+            return p == null || p.cor != cor;
+        }
+
+        public override  bool[,] MovimentosPossiveis() {
+            bool[,] mat = new bool[tabuleiro.linhas, tabuleiro.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+
+            // cima
+            pos.DefinirValores(posicao.linha - 1, posicao.coluna);
+            while (tabuleiro.PosicaoValida(pos) && PoderMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+                if (tabuleiro.peca(pos) != null && tabuleiro.peca(pos).cor != cor) {
+                    break;
+                }
+
+                pos.linha = pos.linha - 1;
+            }
+            
+            // baixo
+            pos.DefinirValores(posicao.linha + 1, posicao.coluna);
+            while (tabuleiro.PosicaoValida(pos) && PoderMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+                if (tabuleiro.peca(pos) != null && tabuleiro.peca(pos).cor != cor) {
+                    break;
+                }
+
+                pos.linha = pos.linha + 1;
+            }
+
+            // direita
+            pos.DefinirValores(posicao.linha, posicao.coluna + 1);
+            while (tabuleiro.PosicaoValida(pos) && PoderMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+                if (tabuleiro.peca(pos) != null && tabuleiro.peca(pos).cor != cor) {
+                    break;
+                }
+
+                pos.coluna = pos.coluna + 1;
+            }
+
+            // esquerda
+            pos.DefinirValores(posicao.linha, posicao.coluna - 1);
+            while (tabuleiro.PosicaoValida(pos) && PoderMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+                if (tabuleiro.peca(pos) != null && tabuleiro.peca(pos).cor != cor) {
+                    break;
+                }
+
+                pos.coluna = pos.coluna - 1;
+            }
+            
+            return mat;
+        }
+
     }
 }
