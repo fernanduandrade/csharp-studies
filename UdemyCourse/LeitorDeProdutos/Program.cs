8f48110f99e1bec11ddae426a28f33e68a801b57
fernanduandrade <onanduandrade@gmail.com>
2021-09-21 20:07:06 -0300

    new: tarefa leitor de csv

---
+++ UdemyCourse/LeitorDeProdutos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using LeitorDeProdutos.Entities;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace LeitorDeProdutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Caminho do arquivo");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using(StreamReader sr = File.OpenText(path)) {
+                while(!sr.EndOfStream) {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string productName = fields[0];
+                    double productPrice = double.Parse(fields[1], CultureInfo.InvariantCulture);
+
+                    list.Add(new Product(productName, productPrice));
+                }
+            }
+
+            var avg = list.Select(product => product.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine($"Preço médio {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+        
+            var names = list
+                .Where(product => product.Price < avg)
+                .OrderByDescending(p => p.Name)
+                .Select(p => p.Name);
+
+
+            foreach(string name in names) {
+                Console.Write(name);
+            }
+        }
+    }
+}
