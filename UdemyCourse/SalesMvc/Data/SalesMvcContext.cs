5a03d77e56b1e133386eaabc0c79f3cf403e66b9
fernanduandrade <onanduandrade@gmail.com>
2021-09-23 21:20:48 -0300

    new: Novas entidades

--- UdemyCourse/SalesMvc/Data/SalesMvcContext.cs
+++ UdemyCourse/SalesMvc/Data/SalesMvcContext.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-
+using SalesMvc.Models;
 namespace SalesMvc.Data
 {
     public class SalesMvcContext : DbContext
@@ -13,6 +9,8 @@ namespace SalesMvc.Data
         {
         }
 
-        public DbSet<SalesMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ UdemyCourse/SalesMvc/Models/Enums/SalesStatus.cs
@@ -0,0 +1,9 @@
+namespace SalesMvc.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Peding = 0,
+        Billed = 1,
+        Canceled = 2        
+    }
+}
