ca16e4bffc4556c4df87a97a0c341ebd2cafa87c
fernanduandrade <onanduandrade@gmail.com>
2021-09-23 21:49:01 -0300

    new: seeding service

---
+++ UdemyCourse/SalesMvc/Data/SeedingService.cs
@@ -0,0 +1,41 @@
+using SalesMvc.Models;
+using SalesMvc.Models.Enums;
+using System.Linq;
+using System;
+
+namespace SalesMvc.Data
+{
+    public class SeedingService
+    {
+        private SalesMvcContext _context;
+
+        public SeedingService(SalesMvcContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if(_context.Department.Any() || 
+                _context.Seller.Any() ||
+                _context.SalesRecord.Any())
+            {
+               return;  // Já tem dados no db 
+            }
+            Department d1 = new Department(1, "Computers");
+            Department d2 = new Department(2, "Makeup");
+
+            Seller s1 = new Seller(1, "Fernando", "nando.ju@gmail.com", new DateTime(1998, 02, 19), 1000.0, d1);
+            Seller s2 = new Seller(2, "Lara", "lara.abreu@gmail.com", new DateTime(2001, 04, 07), 1000.0, d2);
+        
+            SalesRecord sr1 = new SalesRecord(1, new DateTime(2021,08,24), 11000.0, SaleStatus.Billed, s1);
+            SalesRecord sr2 = new SalesRecord(2, new DateTime(2021,09,07), 19000.0, SaleStatus.Peding, s2);
+        
+            _context.Department.AddRange(d1,d2);
+            _context.Seller.AddRange(s1,s2);
+            _context.SalesRecord.AddRange(sr1,sr2);
+        
+        }
+        
+    }
+}
--- UdemyCourse/SalesMvc/Startup.cs
+++ UdemyCourse/SalesMvc/Startup.cs
@@ -30,15 +30,18 @@ namespace SalesMvc
             services.AddDbContext<SalesMvcContext>(
                 options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"), 
                 builder => builder.MigrationsAssembly("SalesMvc")));
+
+            services.AddScoped<SeedingService>();
             
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
